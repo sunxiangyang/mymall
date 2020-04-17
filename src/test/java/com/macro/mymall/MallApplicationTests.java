80eba124d3d57435d1f170de82c39b660d2e9879
somerays <110>
2020-04-17 22:26:07 +0800

    Initial commit

---
+++ src/test/java/com/macro/mymall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macro.mymall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
